4153c6b79b1e4135aef4f533afbb7db2796695c0
Pedro Alexandre Ribeiro <pedro.alex.ribeiro@hotmail.com>
2018-08-17 17:07:36 +0100

    Making things more modular

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,9 @@
 "use strict";
-var stage;
-var boat = null;
-var circles = new Array();
-var info = [false, false, false, false];
-var tweens = [null, null, null, null];
+let stage;
+let boat = null;
+let buoys = null;
+let circles = null;
+let antenna = null;
 
 (function () {
     window.addEventListener("load", main);
@@ -13,74 +13,21 @@ function main(){
     stage = new createjs.Stage("simulator");
     stage.enableMouseOver();
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", tick);
-    let options = new Image();
-    options.src = "antenna.png";
-    let bitmap = new createjs.Bitmap(options.src);
-    bitmap.x = 700;
-    bitmap.y = 300;
-    bitmap.scaleX = -0.15;
-    bitmap.scaleY = 0.15;
-    stage.addChild(bitmap);
-
-    let boats = new createjs.Text("New boat", "20px Georgia", "#000000");
-    boats.x = 800 - (boats.getMeasuredWidth() + 20);
-    boats.y = 20;
-    let hit = new createjs.Shape();
-    hit.graphics.beginFill("#000").drawRect(0, 0, boats.getMeasuredWidth(), boats.getMeasuredHeight());
-    boats.hitArea = hit;
-    boats.shadow = new createjs.Shadow("#000000", 5, 5, 10);
-    boats.on("click", newBoat);
-    boats.on("mouseover", buttonHandler);
-    boats.on("mouseout", buttonHandler);
-    stage.addChild(boats);
-
-
-
-    for (let i = 0; i < 4; i++) {
-        let options = new Image();
-        options.src = "boia.png";
-        let bitmap = new createjs.Bitmap(options.src);
-        bitmap.x = 250;
-        bitmap.y = i*150 + 80;
-        bitmap.scaleX = 0.02;
-        bitmap.scaleY = 0.02;
-        let circle = new createjs.Shape();
-        circle.graphics.beginFill("Grey").drawCircle(0, 0, 75);
-        circle.x = bitmap.x + 5;
-        circle.y = bitmap.y + 10;
-        circle.alpha = 0.2;
+    boat = createBoat();
+    antenna = createAntenna();
+    buoys = new Array();
+    circles = new Array();
+    for(let i = 0; i<4; ++i){
+        let buoy = createBuoy(250, i*150 + 65);
+        let circle = createZone(250, i*150 + 65);
+        buoys.push(buoy);
         circles.push(circle);
+        stage.addChild(buoy);
         stage.addChild(circle);
-        stage.addChild(bitmap);
     }
-}
-
-function newBoat(){
-    let options = new Image();
-    options.src = "boat.png";
-    let bitmap = new createjs.Bitmap(options.src);
-    bitmap.x = -100;
-    bitmap.y = -100;
-    bitmap.scaleX = 0.5;
-    bitmap.scaleY = 0.5;
-    bitmap.rotation = 55 + 180;
-    boat = bitmap;
-    stage.addChild(bitmap);
-    createjs.Tween.get(bitmap, {loop: false})
-        .to({x: 500, y: 800}, 10000, createjs.Ease.none);
-}
-
-function buttonHandler(ev){
-    ev.target.alpha = (ev.type === "mouseover") ? 1 : 0.8;
-    ev.target.shadow = (ev.type === "mouseover") ? new createjs.Shadow("#000000", 15, 15, 10) : new createjs.Shadow("#000000", 5, 5, 10);
-}
-
-function tick(event) {
-    if(boat != null) {
-        calculateCollision(boat, circles);
-    }
-    stage.update(event);
+    stage.addChild(antenna);
+    stage.addChild(boat);
+    createjs.Ticker.addEventListener("tick", tick);
 }
 
 function calculateCollision(){
@@ -89,7 +36,7 @@ function calculateCollision(){
         if (circles[index].hitTest(pt.x, pt.y)) {
             circles[index].alpha = 1;
             if(!info[index]) {
-                createInfo(circles[index].x, circles[index].y, index);
+                createPackage(circles[index].x, circles[index].y, index);
                 info[index] = true;
             }
         }
@@ -104,16 +51,66 @@ function calculateCollision(){
     }
 }
 
-function createInfo(x, y, index){
-    let options = new Image();
-    options.src = "info.png";
-    let bitmap = new createjs.Bitmap(options.src);
-    bitmap.x = x + 20;
-    bitmap.y = y - 42;
-    bitmap.scaleX = 0.1;
-    bitmap.scaleY = 0.1;
-    createjs.Tween.get(bitmap, {loop: true})
-        .to({x: 700, y: 300}, 500, createjs.Ease.none);
-    tweens[index] = bitmap;
-    stage.addChild(bitmap);
+function createPackage(x, y){
+    let pack = new Image();
+    pack.src = "images/info.png";
+    let packBitmap = new createjs.Bitmap(pack.src);
+    packBitmap.x = x + 20;
+    packBitmap.y = y - 42;
+    packBitmap.scaleX = 0.1;
+    packBitmap.scaleY = 0.1;
+    return packBitmap;
+}
+
+function createBoat(){
+    let boat = new Image();
+    boat.src = "images/boat.png";
+    let boatBitmap = new createjs.Bitmap(boat.src);
+    boatBitmap.x = -100;
+    boatBitmap.y = -100;
+    boatBitmap.scaleX = 0.5;
+    boatBitmap.scaleY = 0.5;
+    boatBitmap.rotation = 55 + 180;
+    return boatBitmap;
+}
+
+function createBuoy(x, y){
+    let buoy = new Image();
+    buoy.src = "images/buoy.png";
+    let buoyBitmap = new createjs.Bitmap(buoy.src);
+    buoyBitmap.x = x;
+    buoyBitmap.y = y;
+    buoyBitmap.scaleX = 0.02;
+    buoyBitmap.scaleY = 0.02;
+    return buoyBitmap;
+}
+
+function createZone(x, y){
+    let circle = new createjs.Shape();
+    circle.graphics.beginFill("Grey").drawCircle(0, 0, 75);
+    circle.x = x + 5;
+    circle.y = y + 10;
+    circle.alpha = 0.2;
+    return circle;
+}
+
+function createAntenna(){
+    let antenna = new Image();
+    antenna.src = "images/antenna.png";
+    let antennaBitmap = new createjs.Bitmap(antenna.src);
+    antennaBitmap.x = 700;
+    antennaBitmap.y = 300;
+    antennaBitmap.scaleX = -0.15;
+    antennaBitmap.scaleY = 0.15;
+    return antennaBitmap;
+}
+
+function moveBitmaps(destinationX, destinationY, bitmap, time, loop){
+    createjs.Tween.get(bitmap, {loop: loop})
+        .to({x: destinationX, y: destinationY}, time, createjs.Ease.none);
+}
+
+function tick(event) {
+    //calculateCollision(boat, circles);
+    stage.update(event);
 }
