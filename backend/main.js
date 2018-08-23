3320a50da8584abb5e0a141b366672e61defbab2
Pedro Alexandre Ribeiro <pedro.alex.ribeiro@hotmail.com>
2018-08-23 21:57:55 +0100

    No buttons now

--- backend/main.js
+++ backend/main.js
@@ -4,12 +4,17 @@ let boat = null;
 let buoys = null;
 let circles = null;
 let antenna = null;
+let simulationStart = null;
+let currentTime = null;
+let packages = null;
+let packagesTween = null;
 
 (function () {
     window.addEventListener("load", main);
 }());
 
 function main(){
+    simulationStart = createjs.Ticker.getTime(true);
     stage = new createjs.Stage("simulator");
     stage.enableMouseOver();
     createjs.Ticker.setFPS(60);
@@ -17,16 +22,24 @@ function main(){
     antenna = createAntenna();
     buoys = new Array();
     circles = new Array();
+    packages = new Array();
+    packagesTween = [null, null, null, null];
     for(let i = 0; i<4; ++i){
         let buoy = createBuoy(250, i*150 + 65);
         let circle = createZone(250, i*150 + 65);
+        let pack = createPackage(250, i*150 + 65);
         buoys.push(buoy);
         circles.push(circle);
+        packages.push(pack);
+        stage.addChild(pack);
         stage.addChild(buoy);
         stage.addChild(circle);
     }
     stage.addChild(antenna);
     stage.addChild(boat);
+    boat.x = -100;
+    boat.y = 800;
+    moveBitmaps(500, -100, boat, 10000, false);
     createjs.Ticker.addEventListener("tick", tick);
 }
 
@@ -34,19 +47,19 @@ function calculateCollision(){
     for(let index = 0; index < circles.length; ++index) {
         let pt = boat.localToLocal(100, 0, circles[index]);
         if (circles[index].hitTest(pt.x, pt.y)) {
-            circles[index].alpha = 1;
-            if(!info[index]) {
-                createPackage(circles[index].x, circles[index].y, index);
-                info[index] = true;
+            circles[index].alpha = 0.8;
+            if(!packagesTween[index]) {
+                packages[index].alpha = 1;
+                packagesTween[index] = moveBitmaps(antenna.x, antenna.y, packages[index], 300, true);
             }
         }
         else {
-            info[index] = false;
-            if (tweens[index] != null) {
-                createjs.Tween.removeTweens(tweens[index]);
-                tweens[index].alpha = 0;
+            if (packagesTween[index] != null) {
+                createjs.Tween.removeTweens(packagesTween[index]);
+                packagesTween[index] = null;
+                packages[index].alpha = 0;
             }
-            circles[index].alpha = 0.2
+            circles[index].alpha = 0.2;
         }
     }
 }
@@ -59,6 +72,7 @@ function createPackage(x, y){
     packBitmap.y = y - 42;
     packBitmap.scaleX = 0.1;
     packBitmap.scaleY = 0.1;
+    packBitmap.alpha = 0;
     return packBitmap;
 }
 
@@ -70,7 +84,7 @@ function createBoat(){
     boatBitmap.y = -100;
     boatBitmap.scaleX = 0.5;
     boatBitmap.scaleY = 0.5;
-    boatBitmap.rotation = 55 + 180;
+    boatBitmap.rotation = 130;
     return boatBitmap;
 }
 
@@ -106,11 +120,20 @@ function createAntenna(){
 }
 
 function moveBitmaps(destinationX, destinationY, bitmap, time, loop){
-    createjs.Tween.get(bitmap, {loop: loop})
+    return createjs.Tween.get(bitmap, {loop: loop})
         .to({x: destinationX, y: destinationY}, time, createjs.Ease.none);
 }
 
 function tick(event) {
-    //calculateCollision(boat, circles);
-    stage.update(event);
+    currentTime = createjs.Ticker.getTime(true);
+    if(currentTime - simulationStart <= 11000){
+        calculateCollision(boat, circles);
+        stage.update(event);
+    }else{
+        boat.x = -100;
+        boat.y = 800;
+        simulationStart = createjs.Ticker.getTime(true);
+        moveBitmaps(500, -100, boat, 10000, false);
+    }
+
 }
